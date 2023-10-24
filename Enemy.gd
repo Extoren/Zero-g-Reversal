e3af9ce3c20546aef116fe733694c47f81aacc09
Extoren <sffjo.aarre@gmail.com>
2023-10-24 20:26:50 +0200

    Multiple npc's
    Added two more npc's with avoidance to each other.

--- Enemy.gd
+++ Enemy.gd
@@ -9,8 +9,8 @@ func _physics_process(delta):
 	var next_location = nav_agent.get_next_path_position()
 	var new_velocity = (next_location - current_location).normalized() * SPEED
 	
-	velocity = new_velocity.move_toward(new_velocity, .25)
-	move_and_slide()
+	nav_agent.set_velocity(new_velocity)
+	
 
 func update_target_location(target_location):
 	nav_agent.target_position = target_location
@@ -18,3 +18,8 @@ func update_target_location(target_location):
 
 func _on_navigation_agent_3d_target_reached():
 	print("in range")
+
+
+func _on_navigation_agent_3d_velocity_computed(safe_velocity):
+	velocity = velocity.move_toward(safe_velocity, .25)
+	move_and_slide()
