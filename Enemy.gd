28724977f02c6840105d2a2c1dba3b52716ef5bb
Extoren <sffjo.aarre@gmail.com>
2023-10-24 20:04:13 +0200

    Simple "in range" log npc
    This is of course for the soon to come attack patterns or movements

--- Enemy.gd
+++ Enemy.gd
@@ -9,9 +9,12 @@ func _physics_process(delta):
 	var next_location = nav_agent.get_next_path_position()
 	var new_velocity = (next_location - current_location).normalized() * SPEED
 	
-	velocity = new_velocity
+	velocity = new_velocity.move_toward(new_velocity, .25)
 	move_and_slide()
 
 func update_target_location(target_location):
 	nav_agent.target_position = target_location
 	
+
+func _on_navigation_agent_3d_target_reached():
+	print("in range")
