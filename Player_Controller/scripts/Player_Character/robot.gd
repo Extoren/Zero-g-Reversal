9e5fb78487d7cba284c09c34542ed412799eca17
Extoren <sffjo.aarre@gmail.com>
2023-11-16 19:35:40 +0100

    detection range

--- Player_Controller/scripts/Player_Character/robot.gd
+++ Player_Controller/scripts/Player_Character/robot.gd
@@ -9,6 +9,7 @@ var state_machine
 
 const SPEED = 2.0
 const ATTACK_RANGE = 10.0
+const DETECTION_RANGE = 15.0
 
 @export var player_path : NodePath 
 
@@ -46,15 +47,16 @@ func Hit_Successful(Damage, _Direction: Vector3 = Vector3.ZERO, _Position: Vecto
 func _process(delta):
 	velocity = Vector3.ZERO
 	
-	match state_machine.get_current_node():
-		"Run":
-			# Navigation
-			nav_agent.set_target_position(player.global_transform.origin)
-			var next_nav_point = nav_agent.get_next_path_position()
-			velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
-			rotation.y = lerp_angle(rotation.y, atan2(-velocity.x, -velocity.z), delta * 10.0)
-		"Attack":
-			look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
+	if global_position.distance_to(player.global_position) < DETECTION_RANGE:
+		match state_machine.get_current_node():
+			"Run":
+				# Navigation
+				nav_agent.set_target_position(player.global_transform.origin)
+				var next_nav_point = nav_agent.get_next_path_position()
+				velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+				rotation.y = lerp_angle(rotation.y, atan2(-velocity.x, -velocity.z), delta * 10.0)
+			"Attack":
+				look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
 	
 	# Conditions
 	anim_tree.set("parameters/conditions/attack", _target_in_range())
